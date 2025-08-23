dba16b696e0dd1940c1496b3e27764342a46408d
Héctor Gómez <hector@safe.global>
2025-08-23 15:19:07 +0200

    feat: add some TSDoc (#12)

--- src/domain/repositories/events.repository.interface.ts
+++ src/domain/repositories/events.repository.interface.ts
@@ -1,14 +1,29 @@
 import type { ParsedFeesCollectedEvent } from '@domain/entities/parsed-fees-collected-event.entity.js';
 
-// TODO: add TSDocs
-
 export interface EventsRepository {
+  /**
+   * Stores the given FeesCollected events in the database.
+   * @param events The FeesCollected events to store.
+   */
   storeFeesCollectedEvents(events: ParsedFeesCollectedEvent[]): Promise<void>;
 
+  /**
+   * Sets the last block number for FeesCollected events.
+   * @param blockNumber The last block number to set.
+   */
   setFeesCollectedLastBlock(blockNumber: number): Promise<void>;
 
+  /**
+   * Gets the last block number for FeesCollected events.
+   * @returns The last block number, or null if not found.
+   */
   getFeesCollectedLastBlock(): Promise<number | null>;
 
+  /**
+   * Finds FeesCollected events by integrator.
+   * @param integrator The integrator to filter events by.
+   * @returns The matching FeesCollected events.
+   */
   findFeesCollectedEventsByIntegrator(
     integrator: ParsedFeesCollectedEvent['integrator'],
   ): Promise<ParsedFeesCollectedEvent[]>;
