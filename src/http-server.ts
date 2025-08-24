478738eb5e3cd78e7cfc47ba511dc13e46ae04d5
Hector Gomez <hector@safe.global>
2025-08-25 00:52:24 +0200

    improve error handling

--- src/http-server.ts
+++ src/http-server.ts
@@ -1,14 +1,44 @@
 import { logger } from '@infrastructure/logging/logger.js';
+import express, {
+  type Application,
+  type NextFunction,
+  type Request,
+  type Response,
+} from 'express';
 import { pinoHttp } from 'pino-http';
-import express, { type Application } from 'express';
 
-export async function initHttpServer(): Promise<Application> {
-  const port = process.env.PORT ?? 3000;
-  const server = express();
-  server.use(express.json());
-  server.use(pinoHttp());
-  server.listen(port, () => {
-    logger.info(`[HTTP Server] Listening at http://localhost:${port}`);
-  });
-  return server;
+export class HttpServer {
+  private app: Application;
+  private port: number;
+
+  constructor() {
+    this.port = Number(process.env.PORT) || 3000;
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(pinoHttp());
+  }
+
+  start() {
+    this.app.use(this._errorHandler);
+    this.app.listen(this.port, () => {
+      logger.info(`[HTTP Server] Listening at http://localhost:${this.port}`);
+    });
+  }
+
+  getApp(): Application {
+    return this.app;
+  }
+
+  private _errorHandler(
+    err: any,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): void {
+    logger.error(`[Error Handler]: ${err}`);
+    res.status(err?.status || 500).json({
+      code: err?.code || 'UNKNOWN_ERROR',
+      message: err?.message || 'Internal Server Error',
+    });
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,23 @@
-import { initHttpServer } from '@http-server.js';
 import { db } from '@infrastructure/mongo/db.js';
 import { ChainInstance } from '@chain-instance.js';
 import 'dotenv/config';
 import chainConfigs from './infrastructure/config/chains-config.json' with { type: 'json' };
+import type { Application, Request, Response, NextFunction } from 'express';
+import { HttpServer } from '@http-server.js';
 
 // Improvement note:
 // For the sake of simplicity, chains configuration is static and loaded from a JSON at startup.
 // We could consider using a more flexible configuration system (like a database or environment
 // variables) and validate the configuration data.
-class Server {
-  private instances: ChainInstance[] = [];
-
-  async initialize() {
-    db.initializeDatabase({ uri: process.env.MONGO_URI! });
-    const httpServer = await initHttpServer();
-    this.instances = chainConfigs.map(
-      (chainConfig) => new ChainInstance(chainConfig, httpServer),
-    );
-  }
-
-  async start() {
-    await Promise.all(this.instances.map((instance) => instance.start()));
-  }
-
-  async run() {
-    await this.initialize();
-    await this.start();
-  }
+async function main() {
+  db.initializeDatabase({ uri: process.env.MONGO_URI! });
+  const httpServer = new HttpServer();
+  const instances = chainConfigs.map(
+    // httpServer.app is passed to each ChainInstance to add its routes
+    (chainConfig) => new ChainInstance(chainConfig, httpServer.getApp()),
+  );
+  await Promise.all(instances.map((instance) => instance.start()));
+  httpServer.start();
 }
 
-(async () => {
-  const server = new Server();
-  await server.run();
-})();
+main();
