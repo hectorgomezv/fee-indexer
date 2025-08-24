4fafc3aec1592e054a4f32d4a8c98930d681e0bd
Héctor Gómez <hector@safe.global>
2025-08-25 00:57:27 +0200

    Refactor/extract configuration (#15)
    * feat: add getEventsByIntegrator http endpoint
    
    * refactor: extract configuration
    
    * improve error handling

---
+++ src/chain-instance.ts
@@ -0,0 +1,49 @@
+import { EVMIndexerService } from '@application/services/evm-indexer.service.js';
+import type { ChainConfig } from '@domain/entities/chain-config.entity.js';
+import { EVMIndexerController } from '@infrastructure/api/fees-collected/fees-collected.controller.js';
+import { EVMClient } from '@infrastructure/evm/evm-client.js';
+import { EVMScheduler } from '@infrastructure/jobs/evm-indexer.scheduler.js';
+import { getFeesCollectedLastBlockModel } from '@infrastructure/mongo/models/fees-collected-last-block.model.js';
+import { getFeesCollectedEventModel } from '@infrastructure/mongo/models/parsed-fees-collected-event.model.js';
+import { MongoEventsRepository } from '@infrastructure/mongo/mongo-events.repository.js';
+import type { Application } from 'express';
+
+export class ChainInstance {
+  private scheduler: EVMScheduler;
+  private httpController: EVMIndexerController;
+
+  /**
+   * Creates an instance of the chain-specific services and controllers.
+   * Each chain is associated with an instance of the EVMIndexerService, EVMScheduler, EVMIndexerController, etc.
+   * @param chainConfig The configuration for the EVM chain.
+   * @param httpServer The common HTTP server instance.
+   */
+  constructor(
+    private chainConfig: ChainConfig,
+    private httpServer: Application,
+  ) {
+    const { chainName } = chainConfig;
+    const feesCollectedLastBlockModel =
+      getFeesCollectedLastBlockModel(chainName);
+    const feesCollectedEventModel = getFeesCollectedEventModel(chainName);
+    const repository = new MongoEventsRepository(
+      feesCollectedLastBlockModel,
+      feesCollectedEventModel,
+    );
+    const client = new EVMClient(chainConfig);
+    const service = new EVMIndexerService(chainConfig, client, repository);
+    this.scheduler = new EVMScheduler(service, chainConfig);
+    this.httpController = new EVMIndexerController(service);
+  }
+
+  /**
+   * Starts the chain instance and adds its chain-specific HTTP routes.
+   */
+  async start(): Promise<void> {
+    this.httpServer.use(
+      `/api/${this.chainConfig.chainId}`,
+      this.httpController.router,
+    );
+    await this.scheduler.start();
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
-import { bootstrapIndexers } from '@container.js';
-import { initHttpServer } from '@http-server.js';
 import { db } from '@infrastructure/mongo/db.js';
+import { ChainInstance } from '@chain-instance.js';
 import 'dotenv/config';
+import chainConfigs from './infrastructure/config/chains-config.json' with { type: 'json' };
+import type { Application, Request, Response, NextFunction } from 'express';
+import { HttpServer } from '@http-server.js';
 
-(async () => {
+// Improvement note:
+// For the sake of simplicity, chains configuration is static and loaded from a JSON at startup.
+// We could consider using a more flexible configuration system (like a database or environment
+// variables) and validate the configuration data.
+async function main() {
   db.initializeDatabase({ uri: process.env.MONGO_URI! });
-  const httpServer = await initHttpServer();
-  await bootstrapIndexers(httpServer);
-})();
+  const httpServer = new HttpServer();
+  const instances = chainConfigs.map(
+    // httpServer.app is passed to each ChainInstance to add its routes
+    (chainConfig) => new ChainInstance(chainConfig, httpServer.getApp()),
+  );
+  await Promise.all(instances.map((instance) => instance.start()));
+  httpServer.start();
+}
+
+main();
